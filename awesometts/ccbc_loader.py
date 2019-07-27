ffaaeb3591e673eb3cc981b079a591348f700cd9
lovac42 <lovac42@protonmail.com>
2019-07-27 04:07:49 -0400

    added default voice for runhooks

--- awesometts/ccbc_loader.py
+++ awesometts/ccbc_loader.py
@@ -507,7 +507,13 @@ def speak_hooks():
         if not text: return
         if clean:
             text=strip(text)
+
         preset=config[type].get(name)
+        if not preset:
+            type=config['read_text_type'] or type
+            name=config['read_text_preset']
+            preset=config[type].get(name)
+
         window=aqt.mw.reviewer.web.window()
         if not preset or type=="presets":
             reviewer.selection_handler(text,preset or DEFAULT_PRESET,window)

201d4a784402df90a26c6178da66a90186cd67b7
lovac42 <lovac42@protonmail.com>
2020-03-09 03:43:00 -0400

    Ignore AnkiDroid TTS tag

--- awesometts/gui/reviewer.py
+++ awesometts/gui/reviewer.py
@@ -371,6 +371,9 @@ class Reviewer(object):
                 )
             return
 
+        if svc_id == 'android': #Ignore AnkiDroid TTS tag
+            return
+
         self._addon.router(
             svc_id=svc_id,
             text=text,
